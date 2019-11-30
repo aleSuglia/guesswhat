a39565225493b1fcdc8f603396ca1f1bb346f13c
Alessandro Suglia <alessandro.suglia@gmail.com>
2019-11-30 14:23:51 +0000

    Extract features from questioner model

--- extract_dialogue_features.sh
+++ extract_dialogue_features.sh
@@ -7,7 +7,7 @@ python src/extract_dialogue_features.py \
       -exp_identifier 867d59b933a89f4525b189da9d67f17b \
       -img_dir /scratch/ale_models/guesswhat_devries/ft_vgg_img \
       -config config/qgen/config.json \
-      -dict_file models/out/dict.json \
+      -dict_file data/original/dict.json \
       -no_thread 2
 
 python src/extract_dialogue_features.py \
@@ -16,6 +16,7 @@ python src/extract_dialogue_features.py \
       -config config/qgen/config.json \
       -img_dir /scratch/ale_models/guesswhat_devries/ft_vgg_img \
       -exp_dir /scratch/ale_models/guesswhat_devries/out/loop/ \
+      -dict_file data/original/dict.json \
       -exp_identifier e3a6b91687fa1589696f5969b1433a47 \
       --rl_model \
       -no_thread 2
